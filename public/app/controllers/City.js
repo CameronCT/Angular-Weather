819441655ea69053eebac9ecd4ee621d52826b06
Cameron <gnikgg@hotmail.com>
2017-01-28 06:05:17 -0800

    Added a basic error message if City returns 502

--- public/app/controllers/City.js
+++ public/app/controllers/City.js
@@ -1,12 +1,16 @@
-app.controller('City', ['$rootScope', '$scope', '$route', '$routeParams', '$http', function($rootScope, $scope, $route, $routeParams, $http) {
+app.controller('City', ['$rootScope', '$scope', '$route', '$routeParams', '$http', '$location', function($rootScope, $scope, $route, $routeParams, $http, $location) {
 
     $scope.getCity = function() {
         Query = $routeParams.name;
         URI = 'http://api.openweathermap.org/data/2.5/forecast/daily?units=metric&cnt=7&appid=' + $rootScope.config.api + '&q=';
         $http.get(URI + Query).then(
-        	function(result){
-            	$scope.days 		= result.data;
-        	}
+        	function(result) {
+                $scope.days 		= result.data;
+        	},
+            function(result) {
+                $location.url('/');
+                $rootScope.error        = 'The city you have entered could not be found, please try again!';
+            }
         );
     };
     $scope.getCity();
