0fc30dcac5776a38aa50ce20b569f966b4362183
Cameron <gnikgg@hotmail.com>
2017-01-27 23:09:57 -0800

    Minor bugfixes - Finished

--- public/app/controllers/City.js
+++ public/app/controllers/City.js
@@ -2,7 +2,7 @@ app.controller('City', ['$rootScope', '$scope', '$route', '$routeParams', '$http
 
     $scope.getCity = function() {
         Query = $routeParams.name;
-        URI = 'http://api.openweathermap.org/data/2.5/forecast/daily?units=metric&cnt=21&appid=' + $rootScope.config.api + '&q=';
+        URI = 'http://api.openweathermap.org/data/2.5/forecast/daily?units=metric&cnt=7&appid=' + $rootScope.config.api + '&q=';
         $http.get(URI + Query).then(
         	function(result){
             	$scope.days 		= result.data;
